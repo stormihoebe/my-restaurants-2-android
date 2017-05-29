b4bcb4b258fdb6b3f54985db5f0d78adbed15970
Grace Stuart <gracerstuart@gmail.com>
2017-05-29 10:08:47 -0700

    add final and change string to capital

--- app/src/main/java/com/epicodus/myrestaurants/Constants.java
+++ app/src/main/java/com/epicodus/myrestaurants/Constants.java
@@ -1,8 +1,8 @@
 package com.epicodus.myrestaurants;
 
 public class Constants {
-    public static string YELP_CONSUMER_KEY = BuildConfig.Yelp_Consumer_Key;
-    public static string YELP_CONSUMER_SECRET = BuildConfig.Yelp_Consumer_Secret;
-    public static string YELP_TOKEN = BuildConfig.Yelp_Token;
-    public static string YELP_TOKEN_SECRET = BuildConfig Yelp_Token_Secret;
+    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
+    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
 }
