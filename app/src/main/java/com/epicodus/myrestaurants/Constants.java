b3f8b9e568711e18c064d08562aed421ffbc2c3d
Grace Stuart <gracerstuart@gmail.com>
2017-05-29 10:06:36 -0700

    add api constants

---
+++ app/src/main/java/com/epicodus/myrestaurants/Constants.java
@@ -0,0 +1,8 @@
+package com.epicodus.myrestaurants;
+
+public class Constants {
+    public static string YELP_CONSUMER_KEY = BuildConfig.Yelp_Consumer_Key;
+    public static string YELP_CONSUMER_SECRET = BuildConfig.Yelp_Consumer_Secret;
+    public static string YELP_TOKEN = BuildConfig.Yelp_Token;
+    public static string YELP_TOKEN_SECRET = BuildConfig Yelp_Token_Secret;
+}
