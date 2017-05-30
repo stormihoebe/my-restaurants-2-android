33448b3a624cec9683f5ff00bbd786a3f9e7debc
Grace Stuart <gracerstuart@gmail.com>
2017-05-29 17:27:30 -0700

    import okhttp3.Callback; import okhttp3.HttpUrl; import okhttp3.OkHttpClient; import okhttp3.Request; import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer; import se.akerfeldt.okhttp.signpost.SigningInterceptor;
    git add .

--- app/src/main/java/com/epicodus/myrestaurants/YelpServices.java
+++ app/src/main/java/com/epicodus/myrestaurants/YelpServices.java
@@ -1,9 +1,13 @@
 package com.epicodus.myrestaurants;
 
+import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
 import se.akerfeldt.okhttp.signpost.SigningInterceptor;
+
 
 public class YelpServices {
     public static void findRestaurants(String location, Callback callback) {
@@ -13,5 +17,16 @@ public class YelpServices {
         OkHttpClient client = new OkHttpClient.Builder()
                 .addInterceptor(new SigningInterceptor(consumer))
                 .build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.YELP_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.YELP_LOCATION_QUERY_PARAMETER, location);
+        String url = urlBuilder.build().toString();
+
+        Request request= new Request.Builder()
+                .url(url)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
     }
 }
