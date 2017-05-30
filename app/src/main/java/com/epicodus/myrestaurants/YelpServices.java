c41d5fbe1124a54e8bc93e1193ba0fd26eb79ea2
Grace Stuart <gracerstuart@gmail.com>
2017-05-29 17:19:20 -0700

    create a new OkHttpClient to create and send our request

--- app/src/main/java/com/epicodus/myrestaurants/YelpServices.java
+++ app/src/main/java/com/epicodus/myrestaurants/YelpServices.java
@@ -1,12 +1,17 @@
 package com.epicodus.myrestaurants;
 
 import okhttp3.Callback;
+import okhttp3.OkHttpClient;
 import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
+import se.akerfeldt.okhttp.signpost.SigningInterceptor;
 
 public class YelpServices {
     public static void findRestaurants(String location, Callback callback) {
         OkHttpOAuthConsumer consumer = new OkHttpOAuthConsumer(Constants.YELP_CONSUMER_KEY, Constants.YELP_CONSUMER_SECRET);
         consumer.setTokenWithSecret(Constants.YELP_TOKEN, Constants.YELP_TOKEN_SECRET);
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(new SigningInterceptor(consumer))
+                .build();
     }
 }
-
